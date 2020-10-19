2047138967c50c51b2f81abe10789f1cc1cc9cd7
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-10-18 22:04:20 -0400

    Implemented city search, functional for current weather but not yet for the five day forecast. Does not yet save current city selection to local storage.

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-var cityName = "Detroit";
+var cityName;
 var owapikey = "9f9caf703d8d509f42ad240169e9fa5a";
+
+$("#today-text").text(moment().format("dddd" + ", " + "LL"));
+
+$("#city-button").click(function () {
+    cityName = $("#city-input").val();
+    displayWeather(cityName);
+})
 
 function displayWeather() {
     var openWeather = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=9f9caf703d8d509f42ad240169e9fa5a";
     $.ajax({
         url: openWeather,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
-        $(".cityName").text(response.city.name);
-        $("#wb1").text("Temperature: " + response.list[0].main.temp +"°F")
+        $(".city-name").text(response.city.name);
+        $("#current-weather").text(response.list[0].weather[0].main);
+        $("#current-temp").text("Temperature: " + response.list[0].main.temp + "°F");
+        $("#current-humidity").text("Humidity: " + response.list[0].main.temp + "%");
+        $("#current-wind").text("Wind Speed: " + response.list[0].wind.speed + " MPH");
+        var weather = response.list[0].weather[0].main;
+        if (weather === "Rain") {
+            var icon = $("<i class='fas fa-cloud-rain fa-2x'></i>");
+            $("#weather-icon").html(icon);
+        }
+        if (weather === "Clear") {
+            var icon = $("<i class='fas fa-sun fa-2x'></i>");
+            $("#weather-icon").html(icon);
+        }
+        if (weather === "Clouds") {
+            var icon = $("<i class='fas fa-cloud fa-2x'></i>");
+            $("#weather-icon").html(icon);
+        }
     })
 }
-
-displayWeather();
